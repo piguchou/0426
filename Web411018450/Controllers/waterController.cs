ee4965084b2dad498100cb8e1ad4c46a4383d54e
allen <allen1246li@gmail.com>
2023-05-17 19:02:41 +0800

    0517

---
+++ Web411018450/Controllers/waterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411018450.Controllers
+{
+    public class waterController : Controller
+    {
+        // GET: water
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
